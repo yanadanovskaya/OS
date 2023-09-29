7dcecc223e367a2f37af7915067fa3fd6f85d5cc
yanadanovskaya <91852932+yanadanovskaya@users.noreply.github.com>
2023-09-29 09:58:08 +0300

    Add files via upload

---
+++ Lab2.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+int arraySize;
+int* array;
+
+void findMinMax();
+void findAverage();
+
+int main() {
+
+    cout << "Enter array size: ";
+    cin >> arraySize;
+
+    array = new int[arraySize];
+
+    for (int i = 0; i < arraySize; i++) {
+        cout << "Enter element " << i << ": ";
+        cin >> array[i];
+    }
+
+    thread minMaxThread(findMinMax);
+    thread averageThread(findAverage);
+
+    minMaxThread.join();
+    averageThread.join();
+
+    int min = 0;
+    int max = 0;
+    double average = 0;
+
+    // get results from threads
+
+    array[min] = average;
+    array[max] = average;
+
+    cout << "Modified array: ";
+    for (int i = 0; i < arraySize; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+void findMinMax() {
+
+    int min = array[0];
+    int max = array[0];
+
+    for (int i = 1; i < arraySize; i++) {
+
+        if (array[i] < min) {
+            min = array[i];
+        }
+        if (array[i] > max) {
+            max = array[i];
+        }
+
+        this_thread::sleep_for(chrono::milliseconds(500));
+    }
+
+    cout << "Min: " << min << endl;
+    cout << "Max: " << max << endl;
+
+}
+
+void findAverage() {
+
+    int sum = 0;
+
+    for (int i = 0; i < arraySize; i++) {
+
+        sum += array[i];
+
+        this_thread::sleep_for(chrono::milliseconds(12));
+    }
+
+    double average = static_cast<double>(sum) / arraySize;
+
+    cout << "Average: " << average << endl;
+
+}
